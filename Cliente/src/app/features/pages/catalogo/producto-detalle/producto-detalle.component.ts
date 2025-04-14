3bd4cd00b191073000a7d1516f5a3aa8ebdb251b
MatiasColazo1 <belenjesuslp@gmail.com>
2025-04-14 18:37:42 -0300

    producto detalle

--- Cliente/src/app/features/pages/catalogo/producto-detalle/producto-detalle.component.ts
+++ Cliente/src/app/features/pages/catalogo/producto-detalle/producto-detalle.component.ts
@@ -1,29 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ProductService } from '../../../../services/product.service';
-import { Product } from 'src/app/models/product';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-producto-detalle',
   templateUrl: './producto-detalle.component.html',
   styleUrls: ['./producto-detalle.component.css']
 })
-export class ProductoDetalleComponent implements OnInit {
-  product!: Product; // Propiedad para almacenar el producto
+export class ProductoDetalleComponent {
 
-  constructor(private route: ActivatedRoute, private productService: ProductService) {}
-
-  ngOnInit(): void {
-    this.obtenerProducto();
-  }
-
-  obtenerProducto() {
-    const id = this.route.snapshot.paramMap.get('id'); // Obtener ID desde la URL
-    if (id) {
-      this.productService.getProductById(id).subscribe({
-        next: (data) => (this.product = data),
-        error: (err) => console.error('❌ Error al obtener el producto:', err),
-      });
-    }
-  }
 }
